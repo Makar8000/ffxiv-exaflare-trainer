cb3e2ea14a6ba5ed7257fa9d68f55bdaa3fc599a
Makar8000 <Makar8000@users.noreply.github.com>
2022-05-20 02:55:38 -0500

    trash

--- src/Exaflares.js
+++ src/Exaflares.js
@@ -5,23 +5,18 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import useSound from 'use-sound';
-import { useContainerDimensions } from './util/useContainerDimensions';
+import { useContainerDimensions, useWindowDimensions } from './util/useContainerDimensions';
 import exaImg from './assets/exaflare.png';
 import correctSound from './assets/correct.wav';
 import incorrectSound from './assets/incorrect.wav';
 import { findSafeSpot } from './util/utils';
 
 const Exaflare = (props) => {
-  // TODO: fix this garbage
   const style = {
     transform: `rotate(${props.direction * 45}deg)`
   };
   if (props.marginTop)
     style.marginTop = props.marginTop;
-
-  let w = props.width;
-  w = Math.floor(350 * (props.width / 750));
-  if (w > 350) w = 350;
 
   return <img
     src={exaImg}
@@ -29,16 +24,16 @@ const Exaflare = (props) => {
     draggable={false}
     alt='exaflare'
     style={style}
-    width={`${w}vw`}
-    height="auto"
-
+    width={`${props.width}px`}
+    height={`auto`}
   />
 }
 
 const ExaflaresContainer = (props) => {
   const [Icon, setIcon] = useState({ Component: DangerousIcon, color: 'transparent' });
   const ref = useRef(null);
-  const { width } = useContainerDimensions(ref);
+  const { width, height } = useContainerDimensions(ref);
+  const { ww, wh } = useWindowDimensions();
 
   const [playRight] = useSound(correctSound, { volume: props.volume });
   const [playWrong] = useSound(incorrectSound, { volume: props.volume });
@@ -61,15 +56,22 @@ const ExaflaresContainer = (props) => {
   }
   onClickSfx[safeSpot.key] = onSuccess;
 
+  // TODO: fix this garbage
+  let mh = Math.min(350, Math.floor(350 * (Math.min(752, (752 / 797.562) * (wh - 70) / 752))));
+  let mw = Math.min(350, Math.floor(350 * (width / 752)));
+  let w = Math.min(mh, mw);
+  let mh2 = Math.min(752, (752 / 797.562) * (wh - 70));
+
   return (
-    <Box ref={ref} sx={{ flexGrow: 1 }
-    }>
-      <Grid container
+    <Box ref={ref} sx={{ flex: 1 }}>
+      <Grid
+        container
         columnSpacing={2}
         rowSpacing={0}
         direction="row"
         justifyContent="center"
         alignItems="center"
+        maxWidth={`${mh2}px`}
         style={{ textAlign: "center" }}
       >
         <Grid item zeroMinWidth xs={12}>
@@ -79,13 +81,13 @@ const ExaflaresContainer = (props) => {
           <Typography>{"Front"}</Typography>
         </Grid>
         <Grid item zeroMinWidth xs={6}>
-          <Exaflare onClick={onClickSfx.left} direction={exas.left.dir} width={width} />
+          <Exaflare onClick={onClickSfx.left} direction={exas.left.dir} width={w} />
         </Grid>
         <Grid item zeroMinWidth xs={6}>
-          <Exaflare onClick={onClickSfx.right} direction={exas.right.dir} width={width} />
+          <Exaflare onClick={onClickSfx.right} direction={exas.right.dir} width={w} />
         </Grid>
         <Grid item zeroMinWidth xs={12}>
-          <Exaflare onClick={onClickSfx.rear} direction={exas.rear.dir} width={width} marginTop={-20} />
+          <Exaflare onClick={onClickSfx.rear} direction={exas.rear.dir} width={w} marginTop={-20} />
         </Grid>
         <Grid item zeroMinWidth xs={12}>
           <Typography>{"Rear"}</Typography>
