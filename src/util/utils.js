1930589d1bcfbc20b0ddbff744550536c4c02c54
Makar8000 <Makar8000@users.noreply.github.com>
2022-06-03 13:10:58 -0500

    fix exaflare randomization logic

--- src/util/utils.js
+++ src/util/utils.js
@@ -25,33 +25,15 @@ const genRandomExa = (loc, dirOverride) => {
   }
 }
 
-export const genRandomExas = attemptCount => {
-  let rear = genRandomExa(4);
-
+export const genRandomExas = () => {
   let left;
+  let right;
+  let rear;
   do {
     left = genRandomExa(7);
-  } while (isInvalidLeft(rear, left));
-
-  let right;
-  let counter = 0;
-  do {
-    if (counter >= 8) {
-      console.warn("impossible combination", { rear, left, right });
-      if (attemptCount > 10) {
-        console.error("too many impossible combinations. aborting");
-        break;
-      }
-      return genRandomExas(attemptCount ? attemptCount + 1 : 1);
-    }
-
-    if (right)
-      right = genRandomExa(1, getDirOffset(right.dir + 1));
-    else
-      right = genRandomExa(1);
-    counter++;
-  } while (isInvalidRight(rear, left, right))
-
+    right = genRandomExa(1);
+    rear = genRandomExa(4);
+  } while (findSafeSpot({ right, left, rear }) === undefined);
   return { rear, left, right };
 }
 
@@ -87,49 +69,4 @@ export const findSafeSpot = exas => {
     return ret.pop();
 
   return undefined;
-}
-
-const isInvalidLeft = (rear, left) => {
-  // rear is covering both; rear must be open
-  if (rear.arrows.indexOf(7) >= 0 && rear.arrows.indexOf(1) >= 0) {
-    if (left.arrows.indexOf(3) >= 0)
-      return true;
-  }
-
-  // rear is cardinal; intercard must cover rear
-  if (rear.dir % 2 === 0 && left.dir % 2 === 1) {
-    if (left.arrows.indexOf(3) === -1)
-      return true;
-  }
-
-  // rear is cardinal; card must cover right
-  if (rear.dir % 2 === 0 && left.dir % 2 === 0) {
-    if (left.arrows.indexOf(2) === -1)
-      return true;
-  }
-
-  // rear is covering self; intercard can't face away
-  if (rear.arrows.indexOf(7) >= 0 && rear.arrows.indexOf(1) === -1) {
-    if (left.dir % 2 === 1 && left.arrows.indexOf(3) === -1)
-      return true;
-  }
-
-  return false;
-}
-
-const isInvalidRight = (rear, left, right) => {
-  // rear is cardinal; exactly 1 other card must exist
-  if (rear.dir % 2 === 0) {
-    if (left.dir % 2 === right.dir % 2)
-      return true;
-  }
-
-  // there can't be 3 intercards
-  if (rear.dir % 2 === 1) {
-    if (left.dir % 2 === 1 && right.dir % 2 === 1)
-      return true;
-  }
-
-  // safe spot must exist
-  return findSafeSpot({ right, left, rear }) === undefined;
 }
